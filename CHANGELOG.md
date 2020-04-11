c62be021c40c1111723b1bf7a3e6c309a9fe6fe1
Mark Everett <mark@eighthdoorclub.com>
2020-04-11 14:00:37 +0100

    Now resolve NestDataLoader using  rather than  to support custom loader implementations which are not singletons (single instance scope).

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.23] - 2020-04-11
+
+### Changed
+
+- Now resolve NestDataLoader using `.resolve` rather than `.get` to support custom loader implementations which are not singletons (single instance scope).
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import {
   ExecutionContext,
   Injectable,
   InternalServerErrorException,
-  NestInterceptor
+  NestInterceptor,
 } from "@nestjs/common";
-import { APP_INTERCEPTOR, ModuleRef } from "@nestjs/core";
+import { APP_INTERCEPTOR, ContextIdFactory, ModuleRef } from "@nestjs/core";
 import { GqlExecutionContext } from "@nestjs/graphql";
 import DataLoader from "dataloader";
 import { Observable } from "rxjs";
@@ -44,22 +44,28 @@ export class DataLoaderInterceptor implements NestInterceptor {
     const ctx = GqlExecutionContext.create(context).getContext();
 
     if (ctx[NEST_LOADER_CONTEXT_KEY] === undefined) {
-      ctx[NEST_LOADER_CONTEXT_KEY] = async (
-        type: string
-      ): Promise<NestDataLoader<any, any>> => {
-        if (ctx[type] === undefined) {
-          try {
-            ctx[type] = this.moduleRef
-              .get<NestDataLoader<any, any>>(type, { strict: false })
-              .generateDataLoader();
-          } catch (e) {
-            throw new InternalServerErrorException(
-              `The loader ${type} is not provided` + e
-            );
+      ctx[NEST_LOADER_CONTEXT_KEY] = {
+        contextId: ContextIdFactory.create(),
+        getLoader: (type: string): Promise<NestDataLoader<any, any>> => {
+          if (ctx[type] === undefined) {
+            try {
+              ctx[type] = (async () => {
+                return (
+                  await this.moduleRef.resolve<NestDataLoader<any, any>>(
+                    type,
+                    ctx[NEST_LOADER_CONTEXT_KEY].contextId,
+                    { strict: false }
+                  )
+                ).generateDataLoader();
+              })();
+            } catch (e) {
+              throw new InternalServerErrorException(
+                `The loader ${type} is not provided` + e
+              );
+            }
           }
-        }
-
-        return ctx[type];
+          return ctx[type];
+        },
       };
     }
     return next.handle();
@@ -86,25 +92,25 @@ export const Loader = createParamDecorator(
       );
     }
 
-    return ctx[NEST_LOADER_CONTEXT_KEY](name);
+    return ctx[NEST_LOADER_CONTEXT_KEY].getLoader(name);
   }
 );
 
 // https://github.com/graphql/dataloader/issues/66#issuecomment-386252044
-export const ensureOrder = options => {
+export const ensureOrder = (options) => {
   const {
     docs,
     keys,
     prop,
-    error = key => `Document does not exist (${key})`
+    error = (key) => `Document does not exist (${key})`,
   } = options;
   // Put documents (docs) into a map where key is a document's ID or some
   // property (prop) of a document and value is a document.
   const docsMap = new Map();
-  docs.forEach(doc => docsMap.set(doc[prop], doc));
+  docs.forEach((doc) => docsMap.set(doc[prop], doc));
   // Loop through the keys and for each one retrieve proper document. For not
   // existing documents generate an error.
-  return keys.map(key => {
+  return keys.map((key) => {
     return (
       docsMap.get(key) ||
       new Error(typeof error === "function" ? error(key) : error)
@@ -131,13 +137,13 @@ export abstract class OrderedNestDataLoader<ID, Type>
     options: IOrderedNestDataLoaderOptions<ID, Type>
   ): DataLoader<ID, Type> {
     const defaultTypeName = this.constructor.name.replace("Loader", "");
-    return new DataLoader<ID, Type>(async keys => {
+    return new DataLoader<ID, Type>(async (keys) => {
       return ensureOrder({
         docs: await options.query(keys),
         keys,
         prop: options.propertyKey || "id",
-        error: keyValue =>
-          `${options.typeName || defaultTypeName} does not exist (${keyValue})`
+        error: (keyValue) =>
+          `${options.typeName || defaultTypeName} does not exist (${keyValue})`,
       });
     });
   }
