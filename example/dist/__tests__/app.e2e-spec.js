79360563a7e0ce90328db702fa9fa91ca429d77d
Keisuke Kan <keisuke_kan@caddi.jp>
2020-10-09 18:44:02 +0900

    feat: add example

---
+++ example/dist/__tests__/app.e2e-spec.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const testing_1 = require("@nestjs/testing");
+const supertest_1 = __importDefault(require("supertest"));
+const app_module_1 = require("./../src/app.module");
+describe('AppController (e2e)', () => {
+    let app;
+    beforeEach(async () => {
+        const moduleFixture = await testing_1.Test.createTestingModule({
+            imports: [app_module_1.AppModule],
+        }).compile();
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+    it('/ (GET)', () => {
+        return supertest_1.default(app.getHttpServer())
+            .get('/')
+            .expect(200)
+            .expect('Hello World!');
+    });
+});
+//# sourceMappingURL=app.e2e-spec.js.map
